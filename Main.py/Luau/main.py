7a8d9a77babf7f37e5369d8e9f765b6aec875d62
exvak-main-api <sigmagang2025@gmail.com>
2026-06-17 01:38:31 +0100

    Update main.py

--- Main.py/Luau/main.py
+++ Main.py/Luau/main.py
@@ -3,6 +3,7 @@ from decompiler import Decompiler
 from optimizer import Optimizer
 from namer import Namer
 from reconstructor import Reconstructor
+from function_rebuilder import FunctionRebuilder
 
 
 def load_file(path):
@@ -19,12 +20,14 @@ def main():
     optimizer = Optimizer()
     namer = Namer()
     reconstructor = Reconstructor()
+    function_rebuilder = FunctionRebuilder()
 
     ast_tree = decompiler.build_ast(chunk.main_proto)
 
     ast_tree = optimizer.optimize(ast_tree)
     ast_tree = namer.rename(ast_tree)
     ast_tree = reconstructor.reconstruct(ast_tree)
+    ast_tree = function_rebuilder.rebuild(ast_tree)
 
     output = decompiler.generator.generate(ast_tree)
 
